e8ead7342f239803fdba11643cf76ffa7695e6e8
Alyssa Brown <alyssa.brown@live.com>
2019-01-28 19:12:57 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,7 +7,7 @@ def third_challenge
         },
       :hero => {:name => "Romeo", :age => "15", :status => "alive"
         },
-      :hero_friends => [{name: "Benvolio", age: "17", attribute: "worried"}, {name: "Mercutio", age: "18", attribute: "hot-headed"}]
+      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot-headed"}]
    }, 
    :capulet => {
       :patriarch => {:name => "Lord Capulet", :age => "50"
@@ -16,7 +16,7 @@ def third_challenge
         },
       :heroine => {:name => "Juliet", :age => "15", :status => "alive"
         },
-      :heroine_friends => [{name: "Steven", age: "30", attribute: "confused"},{name: "Nurse", age: "44", attribute: "worried"}]
+      :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"},{name: "Nurse", age: "44", attitude: "worried"}]
    }
   }
 
