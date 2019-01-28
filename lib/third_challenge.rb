2fcf182c383e09c33d3073162ef74d3c776cd7f1
Alyssa Brown <alyssa.brown@live.com>
2019-01-28 19:08:39 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,7 +7,7 @@ def third_challenge
         },
       :hero => {:name => "Romeo", :age => "15", :status => "alive"
         },
-      :hero_friends => []
+      :hero_friends => [{name: "Benvolio", age: "17", attribute: "worried"}, {name: "Mercutio", age: "18", attribute: "hot-headed"}]
    }, 
    :capulet => {
       :patriarch => {:name => "Lord Capulet", :age => "50"
