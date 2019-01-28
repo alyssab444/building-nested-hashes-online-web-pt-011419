f18a7489ec2eabb704cc95d37a80f6ba1ed19b24
Alyssa Brown <alyssa.brown@live.com>
2019-01-28 18:50:44 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,7 @@ def second_challenge
   epic_tragedy = {
     :montague => {:patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => []
       }, 
-    :capulet => {:patriarch => {}, :matriarch => {}, :heroine_friends => []
+    :capulet => {:patriarch => {}, :matriarch => {}, :herione => {}, :heroine_friends => []
       }}
 
   
